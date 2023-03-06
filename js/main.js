57f30d44d85ebab0cea4be488a0fb5d7de209ca3
Alexey7772 <pplus039@gmail.com>
2023-03-06 16:40:34 +0300

    add:adaptive

--- js/main.js
+++ js/main.js
@@ -2,36 +2,60 @@ const navbar = document.querySelector(".navbar");
 const logo = document.querySelector(".logo-svg use");
 const mMenutoogle = document.querySelector(".mobile-menu-toogle");
 const menu = document.querySelector(".mobile-menu");
-
-  const lightModeOn = (event) => {
+const lightModeOn = (event) => {
   navbar.classList.add("navbar-light");
   logo.href.baseVal = "img/sprite.svg#logo";
 };
-  const lightModeOff = (event) => {
+const lightModeOff = (event) => {
   navbar.classList.remove("navbar-light");
   logo.href.baseVal = "img/sprite.svg#logo-light";
 };
-
-
-   const openMenu = (event) => { // функция открывания меню
-   menu.classList.add("is-open"); // вешает класс is-open
-   mMenutoogle.classList.add("close-menu");
-   document.body.style.overflow ="hidden";// запрещаем прокрутку сайта под меню
-   lightModeOn ();
+const openMenu = (event) => {
+  // функция открывания меню
+  menu.classList.add("is-open"); // вешает класс is-open
+  mMenutoogle.classList.add("close-menu");
+  document.body.style.overflow = "hidden"; // запрещаем прокрутку сайта под меню
+  lightModeOn();
 };
-   const closeMenu = (event) => { // функция закрывания меню
-   menu.classList.remove("is-open"); // убирает класс is-open
-   mMenutoogle.classList.remove("close-menu");
-   document.body.style.overflow ="";// возвращает прокрутку сайта под меню
-   lightModeOff();
+const closeMenu = (event) => {
+  // функция закрывания меню
+  menu.classList.remove("is-open"); // убирает класс is-open
+  mMenutoogle.classList.remove("close-menu");
+  document.body.style.overflow = ""; // возвращает прокрутку сайта под меню
+  lightModeOff();
 };
-
-
-   window.addEventListener("scroll",() => {
-   this.scrollY > 1 ? lightModeOn () : lightModeOff ();
-   });
-   
-   mMenutoogle.addEventListener("click",(event) => {
-   event.preventDefault();
-   menu.classList.contains ("is-open") ? closeMenu (): openMenu();
+window.addEventListener("scroll", () => {
+  this.scrollY > 1 ? lightModeOn() : lightModeOff();
+});
+mMenutoogle.addEventListener("click", (event) => {
+  event.preventDefault();
+  menu.classList.contains("is-open") ? closeMenu() : openMenu();
+});
+const swiper = new Swiper(".swiper", {
+  speed: 400,
+  autoHeight: true,
+  loop: true,
+  slidesPerView: 1,
+  navigation: {
+   nextEl: ".slider-button-next",
+   prevEl: ".slider-button-prev",
+},
+  breakpoints: {
+    // when window width is >= 320px
+    576: {
+      slidesPerView: 2,
+    },
+    // when window width is >= 480px
+    768: {
+      slidesPerView: 3,
+    },
+    // when window width is >= 640px
+    1024: {
+      slidesPerView: 4,
+    },
+    // when window width is >= 1200px
+    1200: {
+      slidesPerView: 5,
+    },
+  },
 });
